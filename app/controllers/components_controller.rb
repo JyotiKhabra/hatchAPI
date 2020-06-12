933697105cfa1744bec803bd140fe336247a334d
vagrant <j_khabra11@hotmail.com>
2020-06-12 21:23:05 +0000

    destroy funct in component

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,4 +1,6 @@
 class ComponentsController < ApplicationController
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
 def show
   @components = Component.find(params[:id])
@@ -7,6 +9,23 @@ end
 
 def create
 
+end
+
+def destroy
+  @component = Component.find(params[:id])
+  @component.destroy
+  @components = Component.where(destination_id:  destination_params["destination_id"])
+  # puts @components
+  render json: @components
+
+end
+
+private
+
+def destination_params
+  params.permit(
+    :destination_id
+  )
 end
 
 end
--- app/models/component.rb
+++ app/models/component.rb
@@ -1,3 +1,4 @@
 class Component < ActiveRecord::Base
   belongs_to :destination
+  has_many :component_items, dependent: :destroy
 end
