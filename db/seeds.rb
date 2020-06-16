8e865ebf3207adeaca9587404aec8a4db4e2d696
Stacey Keating <staceyamberkk@gmail.com>
2020-06-16 02:26:01 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,28 @@
 ## Users
 
 user1 = User.new(
-  name: 'John Smith',
-  email: "john.smith@example.com",
+  name: 'Stacey Keating',
+  email: "staceyamberkk@gmail.com",
   password_digest: "password"
 )
 
 user1.save!
 
 user2 = User.new(
-  name: 'Warren Peace',
-  email: "warren.peace@example.com",
+  name: 'Joey Kishiuchi',
+  email: "joey.kishiuchi@example.com",
   password_digest: "password"
 )
 
 user2.save!
 
 user3 = User.new(
-  name: 'Robin Banks'  ,
-  email: "robin.banks@example.com",
+  name: 'Jyoti Khabra',
+  email: "jyoti.khabra@example.com",
   password_digest: "password"
 )
 
 user3.save!
-
-user4 = User.new(
-  name: 'Anita Bathe',
-  email: "anita.bathe@example.com",
-  password_digest: "password"
-)
-
-user4.save!
-
-user5 = User.new(
-  name: 'Al Dente',
-  email: "al.dante@example.com",
-  password_digest: "password"
-)
-
-user5.save!
-
-user6 = User.new(
-  name: 'Kerry Oki',
-  email: "kerry.oki@example.com",
-  password_digest: "password"
-)
-
-user6.save!
 
 ##trips
 
@@ -99,17 +75,22 @@ destination3.save!
 ##collaborators
 
 collaborator1 = Collaborator.new(
-  user_id: 2,
+  user_id: 1,
   trip_id: 1
 )
 collaborator1.save!
 
 collaborator2 = Collaborator.new(
-  user_id: 3,
+  user_id: 2,
   trip_id: 1
 )
 collaborator2.save!
 
+collaborator3 = Collaborator.new(
+  user_id: 3,
+  trip_id: 1
+)
+collaborator2.save!
 
 ##packing_items
 
@@ -131,6 +112,45 @@ packing_item3 = PackingItem.new(
 )
 packing_item3.save!
 
+
+packing_item4 = PackingItem.new(
+  description: "Walking shoes",
+  trip_id: 1
+)
+packing_item4.save!
+
+packing_item5 = PackingItem.new(
+  description: "Formal dress",
+  trip_id: 1
+)
+packing_item5.save!
+
+
+packing_item6 = PackingItem.new(
+  description: "Makeup bag",
+  trip_id: 1
+)
+packing_item6.save!
+
+packing_item7 = PackingItem.new(
+  description: "Headphones",
+  trip_id: 1
+)
+packing_item7.save!
+
+packing_item8 = PackingItem.new(
+  description: "Camera",
+  trip_id: 1
+)
+
+packing_item8.save!
+
+packing_item9 = PackingItem.new(
+  description: "Money belt",
+  trip_id: 1
+)
+
+packing_item9.save!
 ##components
 
 component1 = Component.new(
@@ -152,13 +172,13 @@ component3 = Component.new(
 component3.save!
 
 component4 = Component.new(
-  title: "Parks",
+  title: "Museums",
   destination_id: 1
 )
 component4.save!
 
 component5 = Component.new(
-  title: "Shopping",
+  title: "Markets",
   destination_id: 1
 )
 component5.save!
@@ -202,7 +222,7 @@ component11.save!
 
 ##component_items
 
-##Barcelona seeds
+#barcelona 
 
 component_item2 = ComponentItem.new(
   title: "Disfrutar Barcelona",
@@ -216,7 +236,7 @@ component_item2.save!
 
 component_item3 = ComponentItem.new(
   title: "La Gastronomica",
-  description: "Amazing food, super friendly service.H ighly recommend. Get there early!",
+  description: "Amazing food, super friendly service. Highly recommend. Get there early!",
   image_url: "https://www.google.com/maps/uv?hl=en&pb=!1s0x12a4a27b5dd2a67b%3A0xf87a13373431814d!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMRUmP1g5XFLsV97QbGMGzG82QC_G796PjQl7U4%3Dw130-h87-n-k-no!5srestaurants%20in%20barcelona%20-%20Google%20Search!15sCgIgAQ&imagekey=!1e10!2sAF1QipMRUmP1g5XFLsV97QbGMGzG82QC_G796PjQl7U4#",
   address: "15, Carrer de Vilamarí, 9, 08015 Barcelona, Spain",
   component_id: 6
@@ -309,157 +329,7 @@ component_item12 = ComponentItem.new(
 )
 component_item12.save!
 
-
-
-
-#rome seeds
-
-component_item30 = ComponentItem.new(
-  title: "Vatican City",
-  description: "Home to the pope."
-  image_url: "https://images.pexels.com/photos/3892129/pexels-photo-3892129.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  address: "Città del Vaticano 00120 Vatican City",
-  component_id: 9
-)
-component_item30.save!
-
-component_item31 = ComponentItem.new(
-  title: "Cinque Terre",
-  description: "Would love to rent a boat here."
-  image_url: "https://images.pexels.com/photos/248771/pexels-photo-248771.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  address: "Province of La Spezia",
-  component_id: 9
-)
-component_item31.save!
-
-
-component_item32 = ComponentItem.new(
-  title: "Amalfi Coast",
-  description: "Lots of stairs, we better get in shape!"
-  image_url: "https://images.pexels.com/photos/1296200/pexels-photo-1296200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  address: "Costiera Amalfitana",
-  component_id: 9
-)
-component_item32.save!
-
-
-
-component_item33 = ComponentItem.new(
-  title: "Tuscany",
-  description: "Wine tour?"
-  image_url: "https://images.pexels.com/photos/51947/tuscany-grape-field-nature-51947.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  component_id: 9
-)
-component_item33.save!
-
-
-component_item34 = ComponentItem.new(
-  title: "Borghese",
-  image_url: "https://images.pexels.com/photos/21264/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  address: "Piazzale Scipione Borghese, 5, 00197 Roma RM, Italy",
-  component_id: 10
-)
-component_item34.save!
-
-component_item35 = ComponentItem.new(
-  title: "Pantheon",
-  image_url: "https://images.pexels.com/photos/2676602/pexels-photo-2676602.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  address: "Piazza della Rotonda, 00186 Roma RM, Italy",
-  component_id: 10
-)
-component_item35.save!
-
-component_item35 = ComponentItem.new(
-  title: "National Roman Museum",
-  image_url: "https://images.pexels.com/photos/615344/julius-caesar-rome-roman-empire-615344.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  address: "Viale Enrico de Nicola, 78, 00185 Roma RM, Italy",
-  component_id: 10
-)
-component_item35.save!
-
-
-component_item36 = ComponentItem.new(
-  title: "National Gallery of Modern and Contemporary Art",
-  image_url: "https://images.pexels.com/photos/1573434/pexels-photo-1573434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "I've heard great things about this museum! Something different."
-  component_id: 10
-)
-component_item36.save!
-
-component_item37 = ComponentItem.new(
-  title: "Cantina e Cucina",
-  image_url: "https://images.pexels.com/photos/1487511/pexels-photo-1487511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "Carbonara on my mind."
-  address: "Via del Governo Vecchio, 87, 00186 Roma RM, Italy"
-  component_id: 11
-)
-component_item37.save!
-
-component_item38 = ComponentItem.new(
-  title: "La Tavernaccia",
-  image_url: "https://images.pexels.com/photos/1487511/pexels-photo-1487511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "Black truffle sauce? Yes."
-  address: "Via Giovanni da Castel Bolognese, 63, 00153 Roma RM, Italy"
-  component_id: 11
-)
-component_item38.save!
-
-component_item39 = ComponentItem.new(
-  title: "Roscioli Salumeria con Cucina",
-  image_url: "https://images.pexels.com/photos/1352271/pexels-photo-1352271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "At least for dessert!"
-  address: "Via dei Giubbonari, 21, 00186 Roma RM, Italy"
-  component_id: 11
-)
-component_item39.save!
-
-
-component_item40 = ComponentItem.new(
-  title: "Roscioli Salumeria con Cucina",
-  image_url: "https://images.pexels.com/photos/1398688/pexels-photo-1398688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "There is nothing wrong with eating pasta every day in Italy!"
-  address: "Piazzale Aurelio, 7, 00152 Roma RM, Italy"
-  component_id: 11
-)
-component_item40.save!
-
-#paris seeds
-
-component_item41 = ComponentItem.new(
-  title: "Le Syndicat",
-  image_url: "https://images.pexels.com/photos/1189257/pexels-photo-1189257.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "Mixology spot."
-  address: "51 Rue du Faubourg Saint-Denis, 75010 Paris, France"
-  component_id: 2
-)
-component_item41.save!
-
-component_item42 = ComponentItem.new(
-  title: "Billili",
-  image_url: "https://images.pexels.com/photos/774455/pexels-photo-774455.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=5000",
-  description: "Wine bar."
-  address: "136 Rue du Faubourg Poissonnière, 75010 Paris, France"
-  component_id: 2
-)
-component_item42.save!
-
-component_item43 = ComponentItem.new(
-  title: "Combat",
-  image_url: "https://images.pexels.com/photos/3793319/pexels-photo-3793319.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "Great view."
-  address: "63 Rue de Belleville, 75019 Paris, France"
-  component_id: 2
-)
-component_item43.save!
-
-component_item44 = ComponentItem.new(
-  title: "Lulu White",
-  image_url: "https://images.pexels.com/photos/3771819/pexels-photo-3771819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  description: "Absinthe laced cocktail? Yes please."
-  address: "12 Rue Frochot, 75009 Paris, France"
-  component_id: 2
-)
-component_item44.save!
+#paris 
 
 component_item1 = ComponentItem.new(
   title: "Hardware Societe Paris",
@@ -545,4 +415,216 @@ component_item20 = ComponentItem.new(
 component_item20.save!
 
 
+#rome seeds
 
+component_item30 = ComponentItem.new(
+  title: "Vatican City",
+  description: "Home to the pope.",
+  image_url: "https://images.pexels.com/photos/3892129/pexels-photo-3892129.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  address: "Città del Vaticano 00120 Vatican City",
+  component_id: 9
+)
+component_item30.save!
+
+component_item31 = ComponentItem.new(
+  title: "Cinque Terre",
+  description: "Would love to rent a boat here.",
+  image_url: "https://images.pexels.com/photos/248771/pexels-photo-248771.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  address: "Province of La Spezia",
+  component_id: 9
+)
+component_item31.save!
+
+
+component_item32 = ComponentItem.new(
+  title: "Amalfi Coast",
+  description: "Lots of stairs, we better get in shape!",
+  image_url: "https://images.pexels.com/photos/1296200/pexels-photo-1296200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  address: "Costiera Amalfitana",
+  component_id: 9
+)
+component_item32.save!
+
+
+
+component_item33 = ComponentItem.new(
+  title: "Tuscany",
+  description: "Wine tour?",
+  image_url: "https://images.pexels.com/photos/51947/tuscany-grape-field-nature-51947.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  component_id: 9
+)
+component_item33.save!
+
+
+component_item34 = ComponentItem.new(
+  title: "Borghese",
+  image_url: "https://images.pexels.com/photos/21264/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  address: "Piazzale Scipione Borghese, 5, 00197 Roma RM, Italy",
+  component_id: 10
+)
+component_item34.save!
+
+component_item35 = ComponentItem.new(
+  title: "Pantheon",
+  image_url: "https://images.pexels.com/photos/2676602/pexels-photo-2676602.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  address: "Piazza della Rotonda, 00186 Roma RM, Italy",
+  component_id: 10
+)
+component_item35.save!
+
+component_item35 = ComponentItem.new(
+  title: "National Roman Museum",
+  image_url: "https://images.pexels.com/photos/615344/julius-caesar-rome-roman-empire-615344.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  address: "Viale Enrico de Nicola, 78, 00185 Roma RM, Italy",
+  component_id: 10
+)
+component_item35.save!
+
+
+component_item36 = ComponentItem.new(
+  title: "National Gallery of Modern and Contemporary Art",
+  image_url: "https://images.pexels.com/photos/1573434/pexels-photo-1573434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "I've heard great things about this museum! Something different.",
+  component_id: 10
+)
+component_item36.save!
+
+component_item37 = ComponentItem.new(
+  title: "Cantina e Cucina",
+  image_url: "https://images.pexels.com/photos/1487511/pexels-photo-1487511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Carbonara on my mind.",
+  address: "Via del Governo Vecchio, 87, 00186 Roma RM, Italy",
+  component_id: 11
+)
+component_item37.save!
+
+component_item38 = ComponentItem.new(
+  title: "La Tavernaccia",
+  image_url: "https://images.pexels.com/photos/1487511/pexels-photo-1487511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Black truffle sauce? Yes.",
+  address: "Via Giovanni da Castel Bolognese, 63, 00153 Roma RM, Italy",
+  component_id: 11
+)
+component_item38.save!
+
+component_item39 = ComponentItem.new(
+  title: "Roscioli Salumeria con Cucina",
+  image_url: "https://images.pexels.com/photos/1352271/pexels-photo-1352271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "At least for dessert!",
+  address: "Via dei Giubbonari, 21, 00186 Roma RM, Italy",
+  component_id: 11
+)
+component_item39.save!
+
+
+component_item40 = ComponentItem.new(
+  title: "Roscioli Salumeria con Cucina",
+  image_url: "https://images.pexels.com/photos/1398688/pexels-photo-1398688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "There is nothing wrong with eating pasta every day in Italy!",
+  address: "Piazzale Aurelio, 7, 00152 Roma RM, Italy",
+  component_id: 11
+)
+component_item40.save!
+
+#paris
+
+component_item41 = ComponentItem.new(
+  title: "Le Syndicat",
+  image_url: "https://images.pexels.com/photos/1189257/pexels-photo-1189257.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Mixology spot.",
+  address: "51 Rue du Faubourg Saint-Denis, 75010 Paris, France",
+  component_id: 2
+)
+component_item41.save!
+
+component_item42 = ComponentItem.new(
+  title: "Billili",
+  image_url: "https://images.pexels.com/photos/774455/pexels-photo-774455.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=5000",
+  description: "Wine bar.",
+  address: "136 Rue du Faubourg Poissonnière, 75010 Paris, France",
+  component_id: 2
+)
+component_item42.save!
+
+component_item43 = ComponentItem.new(
+  title: "Combat",
+  image_url: "https://images.pexels.com/photos/3793319/pexels-photo-3793319.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Great view.",
+  address: "63 Rue de Belleville, 75019 Paris, France",
+  component_id: 2
+)
+component_item43.save!
+
+component_item44 = ComponentItem.new(
+  title: "Lulu White",
+  image_url: "https://images.pexels.com/photos/3771819/pexels-photo-3771819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Absinthe laced cocktail? Yes please.",
+  address: "12 Rue Frochot, 75009 Paris, France",
+  component_id: 2
+)
+component_item44.save!
+
+
+component_item45 = ComponentItem.new(
+  title: "Marché aux Fleurs",
+  image_url: "https://images.pexels.com/photos/250716/pexels-photo-250716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Circa 1808 flower market.",
+  address: "37 Place Louis Lépine, 75004 Paris, France",
+  component_id: 5
+)
+component_item45.save!
+
+component_item46 = ComponentItem.new(
+  title: "Les Puces de Saint-Ouen",
+  image_url: "https://images.pexels.com/photos/250716/pexels-photo-250716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "One of the oldest flea markets.",
+  address: "142 Rue des Rosiers, 93400 Saint-Ouen, France",
+  component_id: 5
+)
+component_item46.save!
+
+component_item47 = ComponentItem.new(
+  title: "Le Marché du Livre",
+  image_url: "https://img.theculturetrip.com/1440x960/smart/wp-content/uploads/2019/02/bj8nfx.jpg",
+  description: "Weekly book market, tres cute.",
+  address: "104 Rue Brancion, 75015 Paris, France",
+  component_id: 5
+)
+component_item47.save!
+
+component_item48 = ComponentItem.new(
+  title: "Le Marché Bastille",
+  image_url: "https://images.pexels.com/photos/1070985/pexels-photo-1070985.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  description: "If there's only time for one this is it.",
+  address: "Place de la Bastille, 75011 Paris, France",
+  component_id: 5
+)
+component_item48.save!
+
+
+component_item49 = ComponentItem.new(
+  title: "Louvre",
+  image_url: "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "What's behind Mona Lisa's smile?",
+  address: "Rue de Rivoli, 75001 Paris, France",
+  component_id: 4
+)
+component_item49.save!
+
+component_item50 = ComponentItem.new(
+  title: "Musée d'Orsay",
+  image_url: "https://images.pexels.com/photos/208636/pexels-photo-208636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  description: "Beaux-Arts railway station built between 1898 and 1900 turned art gallery",
+  address: "1 Rue de la Légion d'Honneur, 75007 Paris, France",
+  component_id: 4
+)
+component_item50.save!
+
+component_item51 = ComponentItem.new(
+  title: "Palais de Tokyo",
+  image_url: "https://www.museeum.com/wp-content/uploads/2017/05/Palais_de_Tokyo_@_Paris_31028565970.jpg",
+  description: "Modern and contemporary art",
+  address: "13 Avenue du Président Wilson, 75116 Paris, France ",
+  component_id: 4
+)
+component_item51.save!
