e870647cd01b813b8bd97d197814ca4829405df9
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-07-15 05:04:46 +0000

    Adjusted seeds to table change

--- db/seeds.rb
+++ db/seeds.rb
@@ -261,7 +261,7 @@ component_item2 = ComponentItem.new(
   image_url: "CmRaAAAAsc1lVGL18XdGKC41UUWjpH1MeRHXafaA4wvSCY72aKl59k5A1tN9GSytqUllkubIR23WUy8-yiGeHSG6U7BppBzCkPmCg_R51AO0P35uNAjx2FeePtWCf5SP_Ff23-g-EhDktF2nIqS1TQm90zdIhas4GhSQLHjyJpHd_upQntlW6hElgx81Kg",
   address: "Carrer de Villarroel, 163, 08036 Barcelona, Spain",
   component_id: 6,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 )
 component_item2.save!
 
@@ -272,7 +272,7 @@ component_item3 = ComponentItem.new(
   image_url: "CmRaAAAA7Xc-qPUXP50wIAq8N6zV9ZRAJ4cRMG_FG3usHMjGSxKItOB9Nq7rprAETIFlKmOVQMzj6Gq7aesVPCsnZjsdWzUj3t5q1_zhGqIvNkEohcax7SYu2BHZeHlbmZIpt0VYEhAYR0kMQiPg5WUySsw9G7pdGhQN1-vNr2MfmWblm3NCM-RDP2JcnA",
   address: "15, Carrer de Vilamarí, 9, 08015 Barcelona, Spain",
   component_id: 6,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 )
 component_item3.save!
 
@@ -283,7 +283,7 @@ component_item4 = ComponentItem.new(
   image_url: "CmRaAAAA5fzQkeBc_ejRScGJPrfv1-JnAESkczzNmCNvtmh1NEogfgOY_iYc1denL7v2zcVq3MYAnZHIhx27ZSGPMwcFQWGyFWElc_t1FjK3BqibjKtaPPjqbNKzca9zGn5b0jZ7EhDxpCrdVqgqUXOjdcS2SEiQGhRpGOQ90S-NHQ7Bb8Td5ZuZXDNB3Q",
   address: "Carrer de Veneçuela, 16, 08019 Barcelona, Spain",
   component_id: 6,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item4.save!
@@ -295,7 +295,7 @@ component_item5 = ComponentItem.new(
   image_url: "CmRaAAAAm9KdvDxU7pP5oW01GZGNncgL4Uhzdpk9uYXA66UzOTRg8A1eow_Gd7hUZ_czz_pwROYV5jHnLLEO-GUjQLP7WkXTAeqWrITG-n4Tni4dkWrOOeSGhcXtBxVJOHPOJRcqEhBbkV048juv4YsIpMZj7y-ZGhQ-NqWXkEYRH1ipBXrm6fmOmq7XUA",
   address: "Carrer de Còrsega, 343, 08037 Barcelona, Spain",
   component_id: 6,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 )
 component_item5.save!
 
@@ -306,7 +306,7 @@ component_item6 = ComponentItem.new(
   image_url: "CmRaAAAAYxypEeOQemevCL1GJFVp72El9_ulX8NejKv6f6JlLbYBIfMunEe5zkc-3IXYPxUFKg9oXsG5BNgI4_nHqRFKIa5e7bBGTfLCa1FeVQH8BQYAJSegBVTBZy3g0qvlLepmEhCI5v_ttUAZBj6KtFW3bo8yGhTohPjfRt9gIkDsFiyFx2OAHJ6sVQ",
   address: "Carrer de Mallorca, 401, 08013 Barcelona, Spain",
   component_id: 7,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 )
 component_item6.save!
 
@@ -318,7 +318,7 @@ component_item7 = ComponentItem.new(
   image_url: "CmRaAAAAarSaIGvIfm_3MPt5bvLx3XNfubiXSjelMzHPB96vSo-V6b3b8CBGEh_zTTGWKSanN2gN0dIAHs-77Wk8SX2n574o8MYwtHvog0lnQZHcazxvrBXCli0fSGtdwFdUGjEHEhBc3LaPaTqAuoxWUO_MWeKXGhS7BjmTH1J9WYJGW9qPF1H9YEcxOg",
   address: "08024 Barcelona, Spain",
   component_id: 7,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item7.save!
@@ -329,7 +329,7 @@ component_item8 = ComponentItem.new(
   image_url: "CmRaAAAAoeV7RcB6N4fjCdrcbO149DTx00iprdcK0ZRBmmBlZlL-DmmP3phDBIPPFzxPy017RUjKAG-575PTqMaMLSUj2-PvJ23mxIvuYlZsKcM-GkjRvJFBY0G96tQCcFE8xWayEhAnS8drlh9-M30eBIMFUX4AGhQBuMnsnVPJIZYfz1P5haanfQJYJA",
   address: "08038 Barcelona, Spain",
   component_id: 7,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item8.save!
@@ -340,7 +340,7 @@ component_item9 = ComponentItem.new(
   image_url: "CmRaAAAA4PK2vSXV0vgnth1y-sjf4EcV85A9jiqKJAOmy3Rdw516aWcbyMokpQBCr0HjO2EU_z25FdbAVGPT9wo6-gXDgzn4wakDXQ5gouEhXnwBDL2D-WsJTAcgBi8SNMMU9AK0EhBjbZGPEQ5DCB-VLlyxbdN6GhSPtdJ-RsgAe6L-oelp86RTi_cq2g",
   address: "C. d'Arístides Maillol, 12, 08028 Barcelona, Spain",
   component_id: 7,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item9.save!
@@ -351,7 +351,7 @@ component_item10 = ComponentItem.new(
   image_url: "CmRaAAAAcwFgUVD6OdAXDtJ7Zfp2qnaBXpYFNSp-lw9biyxbtwVgsOEKGNG_FHGXDbdQCct-q-TrBW07NWnPw2eLvRrcFNawqyH5K713lPGZ_aS4rDnXb3XqoFBjoCX5pZXPHmVMEhAEmPd1meBd7lveo9GNUGZyGhT50aN4xOBDS0KXVBFrEk75Igy7rQ",
   address: "Passeig Marítim de la Barceloneta, s/n, 08003 Barcelona, Spain",
   component_id: 8,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item10.save!
@@ -362,7 +362,7 @@ component_item11 = ComponentItem.new(
   image_url:"CmRaAAAAJAgEx3QFvSVfcQ95SOKFFfZVMLutcpOeHHiKD49lwsJwLC8uaaprU-7F7Cyb9hpy5tghcA6w7xiqFDqlIWPtUH0WxnYm8JhIpCshu3mn3iPQgjBfPqsiNQPmhpPGE8vXEhDtpAvjKj8QTC03Ad8vyPISGhRWFwUVwZbAeP5nxKnIgpn3r_M18g",
   address: "Carrer Montcada, 15-23, 08003 Barcelona, Spain",
   component_id: 8,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 )
 component_item11.save!
 
@@ -372,7 +372,7 @@ component_item12 = ComponentItem.new(
   image_url:"CmRaAAAAs4xvbnGW4xJj64zyXBAyqmSCeeQHz6PwDNeMnFzaR36oDpX1gzYqQQ2m8t1Ag7w3hdPeffVHHbE3Kd0Ef5lAAfMkY2D8WXXTgwIx5XYLb-3-e5iYpZAEenr6r3DSy-BIEhCynoGXpmdx27ePKnUgseHaGhT_45S3anOI9eRoh-O7Ulwo6PbjsA",
   address: "Avinguda Diagonal, 686, 08034 Barcelona, Spain",
   component_id: 8,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 )
 component_item12.save!
 
@@ -384,7 +384,7 @@ component_item1 = ComponentItem.new(
   image_url: "CmRaAAAAMUIw5Tt0D-XwzjShmuNn--J1yA15GNEvluG3t70G8jkIHO-Wuyn86iKru_-kaEvg8q3F67X67PuhFyx3DtwGCwhsJJgIBRh25gs6dnaVkjuD6pMGUAPlDXSQa4KLRxzpEhA8Y49n5FlMnRp1s4egnp6UGhRPGPY46w88i1Le4UrU_-YpenKG1Q",
   address: "The Hardware Société Paris, Rue Lamarck, Paris, France",
   component_id: 1,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 )
 component_item1.save!
 
@@ -394,7 +394,7 @@ component_item13 = ComponentItem.new(
   image_url:"CmRaAAAA07P48ck_D210DZ8InN-LQG2XHh2JA9u1xTor4a6n_SKC-7gxmeea758ogRQckvpQe4XJzd8jfXwMzV2KV3_vHEyJNnoWlFy2vIpfGv7gxHnXvkCN941u3WShygkFOKEUEhD4Vlo5x-laraNGOgAcvIsoGhRSlkRZlKiKkwhWAuEUSabYkyPdYQ",
   address: "33-35 Rue Saint-Sébastien, 75011 Paris, France",
   component_id: 1,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item13.save!
@@ -405,7 +405,7 @@ component_item14 = ComponentItem.new(
   image_url: "CmRaAAAAal9S6v9XUjHkaG8tzvBp8fuvnHvPzUl2blcJeIWnPysaF1Yvqb0-A0jenbYhWlXRJlqZV6mJrY_JdIlSn8SwdeMhBFNYTYxeGaRvP8XVolOAueCSh3S0I1ZcBHOtQUKHEhDz2eA-NmJwwwQguF1QGI76GhTQmf86NgxbeFcnRK49GUpWV7RwjQ",
   address: "112 Rue du Faubourg Saint-Honoré, 75008 Paris, France",
   component_id: 1,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item14.save!
@@ -416,7 +416,7 @@ component_item15 = ComponentItem.new(
   image_url: "CmRaAAAA6ewU-vazcc-oUSHBqE48lneW0OsIfM08qp-un-1YpFyXKW-IhIiAbXDJOqpRHK-Q2fIzbEKHtL7uQTe1O4K7DytQhhCH4lWTXmY56sgZF0Mdwj3_HpkmMaYVVtHKqptjEhD5IO-pJLvds7tdD-23oioLGhT3WtKDeBtCu3rd3Kmb7uU82pquuw",
   address: "Monnaie de Paris, 11 Quai de Conti, 75006 Paris, France",
   component_id: 1,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item15.save!
@@ -427,7 +427,7 @@ component_item16 = ComponentItem.new(
   image_url: "CmRaAAAAleMcmFFNAocOwEtQTY8eLzrB_jJUl9WpJk1LeawmTtFpThIe2qOGqLKl8w9yrUpyTDdXLEbvDJO3m8L7w279AtZMg5YN1YB2Wpr3RvedVZiWXza0AncseFhpjQDiaenJEhDBM1GTSFMV52zgQ5iq1A8JGhRgU5DhwDi0dBG_6F59NEKvUDBEzQ",
   address: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
   component_id: 3,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 )
 component_item16.save!
 
@@ -437,7 +437,7 @@ component_item17 = ComponentItem.new(
   image_url: "CmRaAAAAkqBQzQb5I8A7jw4HuTzCYzHTxv9ihx5_6qB3oN7hkT8JRztCl9VksVmMFsnzPw2gu0AEFVe1tiEdlE-rXy0ugoov5BY31u1437I4BMk5-9cqxlSk-cpfUxxZ4VcAJPlEEhBM2sr6M7PVYtxZIrXEmG-tGhT6Elgfbf5G8qZqoZu4V4LWpZBZ6A",
   address: "35 Rue du Chevalier de la Barre, 75018 Paris, France",
   component_id: 3,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item17.save!
@@ -449,7 +449,7 @@ component_item18 = ComponentItem.new(
   image_url: "CmRaAAAAfK_199Oc6Dll5EECqE1cOzUQtu3xErzRWGGwy3i3bIbQzRnF9zO8g4LBSxNlV039VLX08QoM8T1hLKP-zbKzzxCR_dXMf3NtFZqKrR0EyZYNX8btU_F8T14D2XDsAocvEhC9Lw-bsVB9DmWriueiW89yGhSpOTRy7xuLQsBzlzlqiOAqlWuzOg",
   address: "Place Charles de Gaulle, 75008 Paris, France",
   component_id: 3,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item18.save!
@@ -461,7 +461,7 @@ component_item19 = ComponentItem.new(
   image_url: "CmRaAAAAm5g5J0M6ZmodDOfSYQOKJd-Ij9pieQ3Q2eW7Lpu2QPdH-ukwQ7Gs5akOMfNQ2UQkTU_HNYcrEMk1lThENraC-kGV2nbDn3Buo4YKEd-ceKO76Py6KvlwYsTTIpbVKZ0aEhDklpWi-NfqbrQJNF0xe-KJGhR9aRIX-cQS0im72xWTUxje1jW_jw",
   address: "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
   component_id: 3,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item19.save!
@@ -472,7 +472,7 @@ component_item20 = ComponentItem.new(
   image_url:"CmRaAAAASJcA30-DjqztnAHiZTOpioyFid6tlWoJZoDyYwO_xoqVfc_IPqJdoqc25o5dykZuerVEvsZ6j0-uZqA84VzJXo12VVOGWVdKLHURRQJTlUPwQLjmKYyV9chWoa7i1yHtEhAbfFjr3bfTGqGpllAHnhcrGhT79bU3p4gLOyil6KwCPuff-4VqdQ",
   address: "Place d'Armes, 78000 Versailles, France",
   component_id: 3,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item20.save!
@@ -486,7 +486,7 @@ component_item30 = ComponentItem.new(
   image_url: "CmRaAAAAhrT7kw8CuMfsZwRPBMw_9SH5ydKW1z3mWGZAbr4OCu6r-KRj4ZnB5z0CUBoLNS5RZXSXaICEi9FSC0dkh3BoYywlUPGinudziOEJ3W5TIJAWSk5R8NSO6IthE75xhCFKEhBMMuKwLqOx8tu1D6kdi0uuGhRFKaZHD8-w2y4TBTLYc-U1yCy9Ng",
   address: "Città del Vaticano 00120 Vatican City",
   component_id: 9,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item30.save!
@@ -497,7 +497,7 @@ component_item31 = ComponentItem.new(
   image_url: "CmRaAAAAfMIMsO56Z1a08NfvveNqhqJg7OLT90tKY6Oz8vpj11V93LQ4XIUeImMKJ-Or-751d83CSZeN6gE5M210Pj8-VmISk1UhfmG_ZbwQlnhNL4HzPDIror0aqS2eEu30hdzhEhCwLtannQNb4YKma8lXYhwGGhTRWR9layrtKpIWDf57mjcr_ccTCg",
   address: "Province of La Spezia",
   component_id: 9,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 )
 component_item31.save!
 
@@ -508,7 +508,7 @@ component_item32 = ComponentItem.new(
   image_url: "CmRaAAAABvrCO8L4KueFUEyEGAIyaPp27PJbDzzuAAAVJ2j5WNAxn4FqjNnldNHwnTIWbi9pllOY7acN1K3yIDjvsBCGL8Jr-mTky46fR31q1SY8Uo1Y2EcrheiNMvH0S24Q8F2PEhDhRNsfacTlxbbRugUMbpNiGhRv9ZeJ-PHvuSe69eC7x32CujQYxw",
   address: "Costiera Amalfitana",
   component_id: 9,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item32.save!
@@ -520,7 +520,7 @@ component_item33 = ComponentItem.new(
   description: "Wine tour?",
   image_url: "CmRaAAAAGqZaqZL-A6hTKZhP_uSoujSuJmuqfy_wqcGH6Rqo4OtDAFfNURMlduRju0Dqge0M0LIoXEzRDHdSMS5WqE70su88ZtACZDB8kevV8-OL1n-uEEVzg99Gnlp2qFuj0DfOEhCRBiS6L84Muva0omkysjtsGhTqyBFEvrar1GLW8CMMc5bXMnu5YQ",
   component_id: 9,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item33.save!
@@ -531,7 +531,7 @@ component_item34 = ComponentItem.new(
   image_url: "CmRaAAAA-IrSawDeqJ9979NFkvKCmQvMlHpv4yfy5AmeYTpUAJrjHnOFkIO2rANg-Kf2GUUsUCQFqdEvC-YCEfrAoZ5Sk048duAJPSeJQRSl09WqAuAtcCdFX_cI0kTDs__WkfK2EhBmYhsEIDSYhRP6xiI7VnS4GhRriT7jIK5FjZzy-P_uYXB1B9jMSg",
   address: "Piazzale Scipione Borghese, 5, 00197 Roma RM, Italy",
   component_id: 10,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item34.save!
@@ -541,7 +541,7 @@ component_item35 = ComponentItem.new(
   image_url: "CmRaAAAAzZX_n32rfnZXG5IbmKL67dE-BV7d7gF32UZxxPz-eWm5pD8K1ya8WrowEefxbEW1aTTA3KA9wWAtV6zliGe39Vnha8TKrKzYxPSpAW4_MLrSGJprfOrYU84SBkNQksUqEhA8AKgEfM7m2cgXIJp46WMuGhSsZE-9h06KApN4O1366NOwqZrldQ",
   address: "Piazza della Rotonda, 00186 Roma RM, Italy",
   component_id: 10,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item35.save!
@@ -551,7 +551,7 @@ component_item35 = ComponentItem.new(
   image_url: "CmRaAAAAUKxEQbDJfMpxLN4urkCTiPzZdwzc7WzYiX-drSzGQMEbcWoevt163-kd5JuFq35spmm2E1PpMjN17ts5dX4yK_uxs30madD8JCKtBKYBnBZyUX2nFZ7w-UIpTrk67KYIEhClQrLmWfU2XUdyQcNOEg46GhT3jE3iLysNsqayRFp3SQ3IomS_9A",
   address: "Viale Enrico de Nicola, 78, 00185 Roma RM, Italy",
   component_id: 10,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item35.save!
@@ -562,7 +562,7 @@ component_item36 = ComponentItem.new(
   image_url: "CmRaAAAAsaoo-yCIbTKY8K5wKxQveBF5Kov3hHSkBWej4W3zGb3fEm_7YjznfMcOVSdxYiJF81YX4Fupd-YCrgp12nxe4KHqsFDOpsLS2OdDUohL0uOtyyALlYOfq0xgZSH3IlcYEhAy1s--7lQFiH8PJniV14vzGhSBS0drDrRbHf_NOnbNDHWt5KzG2g",
   description: "I've heard great things about this museum! Something different.",
   component_id: 10,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item36.save!
@@ -573,7 +573,7 @@ component_item37 = ComponentItem.new(
   description: "Carbonara on my mind.",
   address: "Via del Governo Vecchio, 87, 00186 Roma RM, Italy",
   component_id: 11,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item37.save!
@@ -584,7 +584,7 @@ component_item38 = ComponentItem.new(
   description: "Black truffle sauce? Yes.",
   address: "Via Giovanni da Castel Bolognese, 63, 00153 Roma RM, Italy",
   component_id: 11,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item38.save!
@@ -596,7 +596,7 @@ component_item40 = ComponentItem.new(
   description: "There is nothing wrong with eating pasta every day in Italy!",
   address: "Piazzale Aurelio, 7, 00152 Roma RM, Italy",
   component_id: 11,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item40.save!
@@ -609,7 +609,7 @@ component_item41 = ComponentItem.new(
   description: "Mixology spot.",
   address: "51 Rue du Faubourg Saint-Denis, 75010 Paris, France",
   component_id: 2,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item41.save!
@@ -620,7 +620,7 @@ component_item42 = ComponentItem.new(
   description: "Wine bar.",
   address: "136 Rue du Faubourg Poissonnière, 75010 Paris, France",
   component_id: 2,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item42.save!
@@ -631,7 +631,7 @@ component_item43 = ComponentItem.new(
   description: "Great view.",
   address: "63 Rue de Belleville, 75019 Paris, France",
   component_id: 2,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item43.save!
@@ -642,7 +642,7 @@ component_item44 = ComponentItem.new(
   description: "Absinthe laced cocktail? Yes please.",
   address: "12 Rue Frochot, 75009 Paris, France",
   component_id: 2,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item44.save!
@@ -654,7 +654,7 @@ component_item45 = ComponentItem.new(
   description: "Circa 1808 flower market.",
   address: "37 Place Louis Lépine, 75004 Paris, France",
   component_id: 5,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item45.save!
@@ -665,7 +665,7 @@ component_item46 = ComponentItem.new(
   description: "One of the oldest flea markets.",
   address: "142 Rue des Rosiers, 93400 Saint-Ouen, France",
   component_id: 5,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item46.save!
@@ -676,7 +676,7 @@ component_item47 = ComponentItem.new(
   description: "Weekly book market, tres cute.",
   address: "104 Rue Brancion, 75015 Paris, France",
   component_id: 5,
-  creator_name: 'Stacey Keating'
+  user_id: 1
 
 )
 component_item47.save!
@@ -687,7 +687,7 @@ component_item48 = ComponentItem.new(
   description: "If there's only time for one this is it.",
   address: "Place de la Bastille, 75011 Paris, France",
   component_id: 5,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item48.save!
@@ -699,7 +699,7 @@ component_item49 = ComponentItem.new(
   description: "What's behind Mona Lisa's smile?",
   address: "Rue de Rivoli, 75001 Paris, France",
   component_id: 4,
-  creator_name: 'Joey Kishiuchi'
+  user_id: 2
 
 )
 component_item49.save!
@@ -710,7 +710,7 @@ component_item50 = ComponentItem.new(
   description: "Beaux-Arts railway station built between 1898 and 1900 turned art gallery",
   address: "1 Rue de la Légion d'Honneur, 75007 Paris, France",
   component_id: 4,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 
 )
 component_item50.save!
@@ -721,6 +721,6 @@ component_item51 = ComponentItem.new(
   description: "Modern and contemporary art",
   address: "13 Avenue du Président Wilson, 75116 Paris, France ",
   component_id: 4,
-  creator_name: 'Jyoti Khabra'
+  user_id: 3
 )
 component_item51.save!
