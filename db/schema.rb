8d7ea2072cedfc11c50b342b877820192610a31f
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-07-15 04:34:49 +0000

    Replaced creator name in component items to user_id foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200715025346) do
+ActiveRecord::Schema.define(version: 20200715042949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20200715025346) do
     t.integer  "component_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "creator_name"
+    t.integer  "user_id"
   end
 
   add_index "component_items", ["component_id"], name: "index_component_items_on_component_id", using: :btree
