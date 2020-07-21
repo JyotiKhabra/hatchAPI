ed3a4bd14c0e93ab793d1b2c51daa68f53c74384
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-07-21 20:13:03 +0000

    Corrected syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :packing_items, only: [:create, :update, :destroy]
     resources :component_items, only: [:create, :show, :destroy]
     resources :components, only: [:create, :show, :update, :destroy ]
-    resources :collaborators, only: [:create, :add destroy]
+    resources :collaborators, only: [:create, :add, :destroy]
   end
   # NOTES
   # The priority is based upon order of creation: first created -> highest priority.
