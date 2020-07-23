8a9c8893603c8226d22912cd7e61e38a918e84fc
vagrant <j_khabra11@hotmail.com>
2020-07-23 17:47:37 +0000

    accepted incoming changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,14 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-<<<<<<< HEAD
-  def create 
-=======
   def create
     @newUser = User.create(user_params)
     render json: @newUser
->>>>>>> 1352f3008d2e361739eb0da60b35e7f6520ab6a0
   end
 
   def show
     @user = User.find(params[:id])
-    @trips = @user.collaborators.map do |collaborator| 
+    @trips = @user.collaborators.map do |collaborator|
       {
         trip:collaborator.trip,
         destinations: collaborator.trip.destinations
@@ -25,7 +21,7 @@ class UsersController < ApplicationController
     render json: @trips
   end
 
-  private 
+  private
 
   def user_params
     params.permit(
